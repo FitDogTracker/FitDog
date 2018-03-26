c7273830503244312111c3cdb96728c66db40ec3
Kristine Laranjo <kristine@Kristines-MacBook-Pro.local>
2018-03-26 14:24:31 -0700

    Added the doginfoviewcontroller

---
+++ ViewControllers/DogInfoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DogInfoViewController.swift
+//  FitDog
+//
+//  Created by Kristine Laranjo on 3/26/18.
+//  Copyright © 2018 Kristine Laranjo. All rights reserved.
+//
+
+import UIKit
+
+class DogInfoViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didTapColor(_ sender: Any) {
+    }
+    
+    @IBAction func didTapAddPhoto(_ sender: Any) {
+    }
+    
+    @IBAction func didTapSubmit(_ sender: Any) {
+    }
+
+}
