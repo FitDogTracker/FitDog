7eb038b8ef19dfad61b1b65eae9840c7979a545f
Coleman <Coleman@Chanels-MacBook-Pro.local>
2018-04-26 17:45:05 -0700

    Added functionality to view weekly goal progress on the home screen (progress bar does not move yet due to auto layout problems)

---
+++ FitDog/Extensions/UIColor+hex.swift
@@ -0,0 +1,55 @@
+//
+//  UIColor+hex.swift
+//  FitDog
+//
+//  Created by Coleman on 4/25/18.
+//  Copyright © 2018 Kristine Laranjo. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    public convenience init?(hexString: String) {
+        let r, g, b, a: CGFloat
+        
+        if hexString.hasPrefix("#") {
+            let start = hexString.index(hexString.startIndex, offsetBy: 1)
+            let hexColor = String(hexString[start...])
+            
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        let newRed = CGFloat(red)/255
+        let newGreen = CGFloat(green)/255
+        let newBlue = CGFloat(blue)/255
+        
+        self.init(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
--- FitDog/Views/DogCell.swift
+++ FitDog/Views/DogCell.swift
@@ -32,6 +32,18 @@ class DogCell: UITableViewCell {
         }
     }
     
+    var goal: Goal? {
+        didSet {
+            showProgress()
+        }
+    }
+    
+    var distanceWalked: Double?{
+        didSet{
+            showProgress()
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -42,51 +54,23 @@ class DogCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
-}
-
-extension UIColor {
-    public convenience init?(hexString: String) {
-        let r, g, b, a: CGFloat
-        
-        if hexString.hasPrefix("#") {
-            let start = hexString.index(hexString.startIndex, offsetBy: 1)
-            let hexColor = String(hexString[start...])
-            
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-                    
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
+    
+    func showProgress() {
+        if let distanceWalked = distanceWalked{
+            var labelText = ((distanceWalked * 100).rounded() / 100).description
+            if let goal = goal{
+                let weeklyDistanceGoal = goal.distance * 7
+                labelText += "/" + weeklyDistanceGoal.description
+                var progressWidth = progressBackgroundView.frame.width * CGFloat(distanceWalked / weeklyDistanceGoal)
+                if(progressWidth > progressBackgroundView.frame.width){// make sure not to go oversized
+                    progressWidth = progressBackgroundView.frame.width
                 }
+                print("progress Width" + progressWidth.description)
+                progressForegroundView.frame = CGRect(x: progressForegroundView.frame.minX, y: progressForegroundView.frame.minY, width: progressWidth, height: progressForegroundView.frame.height)
             }
+            labelText += " km"
+            completionLabel.text = labelText
         }
-        
-        return nil
     }
-    
-    convenience init(red: Int, green: Int, blue: Int) {
-        let newRed = CGFloat(red)/255
-        let newGreen = CGFloat(green)/255
-        let newBlue = CGFloat(blue)/255
-        
-        self.init(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
-    }
-    
-    func toHexString() -> String {
-        var r:CGFloat = 0
-        var g:CGFloat = 0
-        var b:CGFloat = 0
-        var a:CGFloat = 0
-        getRed(&r, green: &g, blue: &b, alpha: &a)
-        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
-        return String(format:"#%06x", rgb)
-    }
+
 }
