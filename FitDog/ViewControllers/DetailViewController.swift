59eac44c6d5e19fc0da0e8eaa78882430ebb86b2
Coleman <Coleman@Chanels-MacBook-Pro.local>
2018-04-30 16:38:31 -0700

    App now shows dog's name and progress bar in detail view

--- FitDog/ViewControllers/DetailViewController.swift
+++ FitDog/ViewControllers/DetailViewController.swift
@@ -12,9 +12,11 @@ import ParseUI
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var DogNameLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var completionLabel: UILabel!
     @IBOutlet weak var progressBarView: UIView!
+    @IBOutlet weak var progressBackgroundView: UIView!
     @IBOutlet weak var weeklyProgressLabel: UILabel!
     @IBOutlet weak var mondayView: UIView!
     @IBOutlet weak var tuesdayView: UIView!
@@ -40,6 +42,8 @@ class DetailViewController: UIViewController {
         profileImageView.clipsToBounds = true
         profileImageView.image = photoImage
         completionLabel.text = makeCompletionLabelText()
+        progressBarView.frame = CGRect(x: progressBarView.frame.minX, y: progressBarView.frame.minY, width: calculateProgressWidth(), height: progressBarView.frame.height)
+        DogNameLabel.text = dog.name
     }
     
     func makeCompletionLabelText() -> String{
@@ -52,6 +56,15 @@ class DetailViewController: UIViewController {
         var distOverGoal = distanceWalked - weeklyGoal
         distOverGoal = (distOverGoal * 100).rounded() / 100
         return distOverGoal.description + "km over goal"
+    }
+    
+    func calculateProgressWidth() -> CGFloat{
+        let weeklyGoal = CGFloat(goal.distance * 7)
+        let width = progressBackgroundView.frame.width * (CGFloat(distanceWalked) / weeklyGoal)
+        if(width >= progressBackgroundView.frame.width){
+            return progressBackgroundView.frame.width
+        }
+        return width
     }
     
     override func didReceiveMemoryWarning() {
